6c935628f2b0d109cfefe0702af9cd8f0706be26
Datsuka <49899602+datsuka-qwerty@users.noreply.github.com>
2024-09-06 04:01:31 +0900

    [update]Powershell profile
    Add Linux like command alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,19 @@
-oh-my-posh init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/craver.omp.json" | Invoke-Expression
+# oh-my-posh settings
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/craver.omp.json | Invoke-Expression
+
+# User alias
+Set-Alias touch New-Item
+Set-Alias grep findstr
+Set-Alias vim "C:\Program Files\Vim\vim91\vim.exe"
+Set-Alias git "C:\Users\rento\AppData\Local\GitHubDesktop\app-3.4.3\resources\app\git\mingw64\bin\git.exe"
+
+function git() {
+        . $HOME\AppData\Local\GitHubDesktop\app-3.4.3\resources\app\git\mingw64\bin\git.exe
+}
+function cdd() {
+        cd $HOME
+}
+function which($commandname) {
+        Get-Command $commandname | Select-Object -ExpandProperty Definition
+}
